ccab4b5f45e845820b49836accd6ba739b23ff3f
Ashik-Ahammad <ashik19ahammad@gmail.com>
2026-03-08 01:26:59 +0600

    Add Modal

--- scripts/home.js
+++ scripts/home.js
@@ -11,7 +11,7 @@ const getIssueCard = () => {
         countElement.innerText = result.data.length;
       }
       displayCards(result.data);
-    })
+    });
 };
 
 const displayCards = (cards) => {
@@ -21,9 +21,12 @@ const displayCards = (cards) => {
   cards.forEach((card) => {
     const div = document.createElement("div");
     div.classList.add("h-full");
+    div.addEventListener("click", () => {
+      openModal(card);
+    });
 
     div.innerHTML = `
-    <div class="card bg-base-100 w-full h-full shadow-sm border-t-5  flex flex-col ${card.status === 'open' ? 'border-t-[#00A96E]' : 'border-t-[#A855F7]'}">
+    <div class="card cursor-pointer bg-base-100 w-full h-full shadow-sm border-t-5  flex flex-col ${card.status === "open" ? "border-t-[#00A96E]" : "border-t-[#A855F7]"}">
         
         <div class="flex justify-between items-center mx-5 mt-4">
           <div><img src="assets/Open-Status.png" alt="status" class="w-7" /></div>
@@ -45,10 +48,15 @@ const displayCards = (cards) => {
           
           <div class="card-actions justify-start mt-2">
             ${card.labels.map((label) => `
-                <div class="badge badge-soft text-xs">
-                    ${label}
-                </div>`).join("")}
-          </div>
+            <div class="badge badge-outline gap-1 py-3 px-3 border-none" 
+                style="color: ${label.toLowerCase() === "bug" ? "#EF4444" : "#F59E0B"}; 
+                background-color: ${label.toLowerCase() === "bug" ? "#FEF2F2" : "#FFFBEB"}">
+                <span class="text-[10px] font-bold">${label.toUpperCase()}</span>
+            </div>
+            `,
+         )
+            .join("")}
+            </div>
         </div>
 
         <div class="px-5 py-3 text-xs text-gray-500">
@@ -63,19 +71,75 @@ const displayCards = (cards) => {
   manageSpinner(false);
 };
 
+// Spinner
 const manageSpinner = (status) => {
-    if(status == true){
-        
-        document.getElementById("spinner").classList.remove("hidden");
-        document.getElementById("issue-cards-section").classList.add("hidden");
-    }
-    else{
-        
-        document.getElementById("spinner").classList.add("hidden");
-        document.getElementById("issue-cards-section").classList.remove("hidden");
-    }
-}
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("issue-cards-section").classList.add("hidden");
+  } else {
+    document.getElementById("spinner").classList.add("hidden");
+    document.getElementById("issue-cards-section").classList.remove("hidden");
+  }
+};
+
+// Modal data
+const openModal = (card) => {
+  const modal = document.getElementById("issue_modal");
+  const modalBox = modal.querySelector(".modal-box");
+
+  modalBox.innerHTML = `
+        <div class="space-y-4 text-left">
+            <h1 class="text-2xl font-bold text-gray-800">${card.title}</h1>
+            
+            <div class="flex items-center gap-2 text-sm text-gray-500">
+                <div class="badge text-white border-none py-3" 
+                     style="background-color: ${card.status === "open" ? "#00A96E" : "#A855F7"}">
+                    ${card.status === "open" ? "Opened" : "Closed"}
+                </div>
+                <span>• Opened by <span class="font-medium">${card.author}</span> • ${new Date(card.createdAt).toLocaleDateString()}</span>
+            </div>
+
+            <div class="flex gap-2">
+            ${card.labels
+              .map(
+                (label) => `
+                <div class="badge badge-outline gap-1 py-3 px-3 border-none" 
+                    style="color: ${label.toLowerCase() === "bug" ? "#EF4444" : "#F59E0B"}; 
+                    background-color: ${label.toLowerCase() === "bug" ? "#FEF2F2" : "#FFFBEB"}">
+                    <span class="text-[10px] font-bold">${label.toUpperCase()}</span>
+                </div>
+                    `,
+              )
+              .join("")}
+            </div>
+
+            <p class="text-gray-500 leading-relaxed pt-4">
+                ${card.description}
+            </p>
+
+            <div class="grid grid-cols-2 gap-4 bg-gray-50 p-6 rounded-xl mt-6">
+                <div>
+                    <p class="text-gray-400 text-sm mb-1">Assignee:</p>
+                    <p class="font-bold text-gray-800">${card.assignee || "Unassigned"}</p>
+                </div>
+                <div>
+                    <p class="text-gray-400 text-sm mb-1">Priority:</p>
+                    <div class="badge text-white border-none font-bold px-4" 
+                         style="background-color: ${card.priority.toLowerCase() === "high" ? "#EF4444" : card.priority.toLowerCase() === "medium" ? "#F59E0B" : "#9CA3AF"}">
+                        ${card.priority.toUpperCase()}
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-action">
+                <form method="dialog">
+                    <button class="btn bg-[#6419E6] hover:bg-[#4d12b1] text-white border-none px-8">Close</button>
+                </form>
+            </div>
+        </div>
+    `;
+
+  modal.showModal();
+};
 
 getIssueCard();
-
-
